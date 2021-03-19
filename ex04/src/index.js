3f95316d387d77d2f311f2bd8209be730a8cf1ea
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-19 22:47:40 +0100

    Update index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,26 +1,27 @@
+// Only change code below this line
 function sumFibonacci(num) {
   if (num == 1) {
     return 1;
-  } else {
-    var fib0 = 0;
-    var fib1 = 1;
-    var fib = 1;
-    var sum = fib0;
-    while (fib <= num) {
-      if (fib % 2) {
-        sum += fib1;
-      }
-      fib = fib0 + fib1;
-      fib1 += fib0;
-      fib0 = fib1 - fib0;
-    }
   }
-  return sum;
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
 }
+// Only change code above this line
 
 console.log(sumFibonacci(1));
 console.log(sumFibonacci(10));
 console.log(sumFibonacci(20));
 console.log(sumFibonacci(4));
 console.log(sumFibonacci(-5));
+
 module.exports = sumFibonacci;
