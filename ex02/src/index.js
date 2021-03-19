c432f9e2a838a88a1ee2b925616423e33e49af74
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-19 22:43:01 +0100

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,16 +1,16 @@
+// Only change code below this line
 function myCounter(n) {
   if (n < 1) {
     return [];
   } else {
     const countArray = myCounter(n - 1);
-
-    countArray.push(n);
-
+    countArray.unshift(n);
     return countArray;
   }
 }
-console.log(myCounter(-1).reverse());
-console.log(myCounter(10).reverse());
-console.log(myCounter(5).reverse());
+// Only change code above this line
 
+console.log(myCounter(-1));
+console.log(myCounter(10));
+console.log(myCounter(5));
 module.exports = myCounter;
