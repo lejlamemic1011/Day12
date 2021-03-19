e0323db3e07b3d9a7f08402cc79d452bfcd9b9fc
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-19 22:44:41 +0100

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,15 +1,18 @@
+// Only change code below this line
 function rangeOfNumbers(startN, endN) {
-  if (endN - startN === 0) {
+  if (startN == endN) {
     return [startN];
   } else if (startN > endN) {
     return "The starting number will always be less than or equal to the ending number";
   } else {
-    var number = rangeOfNumbers(startN, endN - 1);
-    number.push(endN);
-    return number;
+    const rangeArray = rangeOfNumbers(startN, endN - 1);
+    rangeArray.push(endN);
+    return rangeArray;
   }
 }
-console.log(rangeOfNumbers(1, 4));
+// Only change code above this line
+
+console.log(rangeOfNumbers(1, 7));
 console.log(rangeOfNumbers(3, 10));
 console.log(rangeOfNumbers(5, 5));
 console.log(rangeOfNumbers(7, 5));
