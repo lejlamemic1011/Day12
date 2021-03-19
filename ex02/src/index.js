4f1f8bfb7114c4f749480a474c5db1d9a044791e
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-19 20:18:55 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,16 @@
+function myCounter(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = myCounter(n - 1);
 
+    countArray.push(n);
+
+    return countArray;
+  }
+}
+console.log(myCounter(-1).reverse());
+console.log(myCounter(10).reverse());
+console.log(myCounter(5).reverse());
+
+module.exports = myCounter;
