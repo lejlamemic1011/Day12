ed9bd84e2f5b1da7d791d610fc034b8000d65fbb
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-19 20:19:14 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,16 @@
-
+function rangeOfNumbers(startN, endN) {
+  if (endN - startN === 0) {
+    return [startN];
+  } else if (startN > endN) {
+    return "The starting number will always be less than or equal to the ending number";
+  } else {
+    var number = rangeOfNumbers(startN, endN - 1);
+    number.push(endN);
+    return number;
+  }
+}
+console.log(rangeOfNumbers(1, 4));
+console.log(rangeOfNumbers(3, 10));
+console.log(rangeOfNumbers(5, 5));
+console.log(rangeOfNumbers(7, 5));
+module.exports = rangeOfNumbers;
